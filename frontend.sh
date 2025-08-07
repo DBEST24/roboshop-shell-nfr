ed4a03c8c0b9aa5fa9bbe978f73a08edd000ef77
Darshak24 <117837412+Darshak24@users.noreply.github.com>
2025-08-07 09:05:24 -0400

    Change on server

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,8 @@
-source common.sh
-component="frontend"
 dnf module disable nginx -y
 dnf module enable nginx:1.24 -y
 dnf install nginx -y
-
+source common.sh
+component="frontend"
 APP_PREREQ
 
 # create nginx file
