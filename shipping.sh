536f256c2f8a1bbc1ec05055e4f9cfb60d5e4610
Darshak24 <117837412+Darshak24@users.noreply.github.com>
2025-08-06 20:30:21 -0400

    Change on server

--- shipping.sh
+++ shipping.sh
@@ -8,8 +8,10 @@ source common.sh
 component=shipping
 MAVEN
 
-  dnf install mysql -y
-  mysql -h mysql-dev.dbest24.ca -uroot -pS1 < /app/db/schema.sql
-  mysql -h mysql-dev.dbest24.ca -uroot -pS1 < /app/db/app-user.sql
-  mysql -h mysql-dev.dbest24.ca -uroot -pS1 < /app/db/master-data.sql
+dnf install mysql -y
+
+# shellcheck disable=SC2034
+for file in schema app-user master-data; do
+  mysql -h mysql-dev.dbest24.ca -uroot -pS1 < /app/db/file.sql
+done
 
