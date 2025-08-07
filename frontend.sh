91252b5c0d177ecfb7a7d157fbd6e4df7bf1606d
Darshak24 <117837412+Darshak24@users.noreply.github.com>
2025-08-07 09:03:07 -0400

    Change on server

--- frontend.sh
+++ frontend.sh
@@ -5,10 +5,8 @@ dnf module enable nginx:1.24 -y
 dnf install nginx -y
 
 APP_PREREQ
-source common.sh
-# shellcheck disable=SC2034
-component=nginx
-# create nginx file
-SYSTEMD
 
+# create nginx file
+systemctl enable nginx
+systemctl restart nginx
 
