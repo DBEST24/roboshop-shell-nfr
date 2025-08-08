d7cea9e5b44b8de239f968462121d066af074b41
Darshak24 <117837412+Darshak24@users.noreply.github.com>
2025-08-08 09:50:05 -0400

    Change on server

--- common.sh
+++ common.sh
@@ -3,7 +3,7 @@ APP_PREREQ(){
   useradd roboshop
   rm -rf /app
   mkdir /app
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip
+  curl  -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip
   cd /app
   unzip /tmp/${component}.zip
 
