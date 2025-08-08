6759d6a32bf05fbcee7e2263a6f4093afac98c76
Darshak24 <117837412+Darshak24@users.noreply.github.com>
2025-08-08 10:00:46 -0400

    Change on server

--- catalogue.sh
+++ catalogue.sh
@@ -1,7 +1,7 @@
 source common.sh
 component=catalogue
 NODEJS
-
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y
 mongosh --host mongodb-dev.dbest24.ca </app/db/master-data.js
 
